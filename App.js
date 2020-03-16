edacc60619da2c3d2fdd581ab5acc048b54e6408
chrisxkoch <52881970+chrisxkoch@users.noreply.github.com>
2020-03-16 12:05:28 +0100

    Task 5.8
    Had to redo everything. Working now.

--- App.js
+++ App.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Start from './components/Start';
-import Chat from './components/Chat';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, ScrollView } from 'react-native';
+// import react Navigation
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import KeyboardSpacer from 'react-native-keyboard-spacer';
 
+// import the screens
+import Start from './components/Start.js';
+import Chat from './components/Chat.js';
 
-// local setup fixing errors
-// you must first run the code in the chatApp directory 
-// npm install base-64 when it shows that the variable atob() and btoa() are missing.
-//https://www.npmjs.com/package/base-64
-
-import { decode, encode } from 'base-64'
-
-if (!global.btoa) {
-  global.btoa = encode;
-}
-
-if (!global.atob) {
-  global.atob = decode;
-}
-
+// Create the navigator
 const navigator = createStackNavigator({
-  Start: { screen: Start },
-  Chat: { screen: Chat }
+  Start: {
+    screen: Start,
+    navigationOptions: {
+      header: null,
+    }, 
+  },
+  Chat: { screen: Chat },
 });
 
 const navigatorContainer = createAppContainer(navigator);
-
+// Export it as the root component
 export default navigatorContainer;
--- components/Start.js
+++ components/Start.js
@@ -25,54 +25,54 @@ export default class Start extends Component {
   render() {
     return (
 
-      <ImageBackground source={require('../assets/Background-Image.png')} style={styles.backgroundImage}>
-        {/*App title*/}
-        <Text style={styles.appTitle}>ChatApp</Text>
-        {/*login container*/}
-        <View style={styles.logIn}>
-          {/*textinput form*/}
-          <TextInput
-            style={styles.userNameInput}
-            onChangeText={(userName) => this.setState({ userName })}
-            value={this.state.userName}
-            placeholder='Your Name...'
-          />
-          {/*choose backgroundcolor*/}
-          <Text style={styles.choseBackgroundColor}>Choose Background Color:</Text>
-          {/*wrapper for the different available background colors*/}
-          <View style={styles.backgroundColorWrapper}>
-            {/*black*/}
-            <TouchableOpacity onPress={() => { this.setState({ backgroundColor: '#090C08' }) }}>
-              <View style={[styles.backgroundColor, styles.black]} >
-              </View>
-            </TouchableOpacity>
-            {/*purple*/}
-            <TouchableOpacity onPress={() => { this.setState({ backgroundColor: '#474056' }) }}>
-              <View style={[styles.backgroundColor, styles.purple]}>
-              </View>
-            </TouchableOpacity>
-            {/*gray*/}
-            <TouchableOpacity onPress={() => { this.setState({ backgroundColor: '#8A95A5' }) }}>
-              <View style={[styles.backgroundColor, styles.gray]}>
-              </View>
-            </TouchableOpacity>
-            {/*green*/}
-            <TouchableOpacity onPress={() => { this.setState({ backgroundColor: '#B9C6AE' }) }}>
-              <View style={[styles.backgroundColor, styles.green]}>
-              </View>
-            </TouchableOpacity>
+        <ImageBackground source={require('../assets/Background-Image.png')} style={styles.backgroundImage}>
+          {/*App title*/}
+          <Text style={styles.appTitle}>ChatApp</Text>
+          {/*login container*/}
+          <View style={styles.logIn}>
+            {/*textinput form*/}
+            <TextInput
+              style={styles.userNameInput}
+              onChangeText={(userName) => this.setState({userName})}
+              value={this.state.userName}
+              placeholder='Your Name...'
+            />
+            {/*choose backgroundcolor*/}
+            <Text style={styles.choseBackgroundColor}>Choose Background Color:</Text>
+            {/*wrapper for the different available background colors*/}
+            <View style={styles.backgroundColorWrapper}>
+              {/*black*/}
+              <TouchableOpacity onPress={()=> { this.setState({ backgroundColor: '#090C08' }) }}>
+                <View style={[styles.backgroundColor, styles.black]} >
+                </View>
+              </TouchableOpacity>
+              {/*purple*/}
+              <TouchableOpacity onPress={()=> { this.setState({ backgroundColor: '#474056' }) }}>
+                <View style={[styles.backgroundColor, styles.purple]}>
+                </View>
+              </TouchableOpacity>
+              {/*gray*/}
+              <TouchableOpacity onPress={()=> { this.setState({ backgroundColor: '#8A95A5' }) }}>
+                <View style={[styles.backgroundColor, styles.gray]}>
+                </View>
+              </TouchableOpacity>
+              {/*green*/}
+              <TouchableOpacity onPress={()=> { this.setState({ backgroundColor: '#B9C6AE' }) }}>
+                <View style={[styles.backgroundColor, styles.green]}>
+                </View>
+              </TouchableOpacity>
+            </View>
+            {/*start chatting button --> directs you to conversation screen*/}
+            <Button
+              onPress={() => {
+                this.props.navigation.navigate('Chat', { userName: this.state.userName, backgroundColor: this.state.backgroundColor });
+              }}
+              title="Start Chatting"
+              color = '#757083'
+              style={styles.startChatButton}
+            />
           </View>
-          {/*start chatting button --> directs you to conversation screen*/}
-          <Button
-            onPress={() => {
-              this.props.navigation.navigate('Chat', { userName: this.state.userName, backgroundColor: this.state.backgroundColor });
-            }}
-            title="Start Chatting"
-            color='#757083'
-            style={styles.startChatButton}
-          />
-        </View>
-      </ImageBackground>
+        </ImageBackground>
     );
   }
 }
@@ -134,7 +134,7 @@ const styles = StyleSheet.create({
   backgroundColor: {
     height: 40,
     width: 40,
-    borderRadius: 40 / 2,
+    borderRadius: 40/2,
     marginLeft: 15
   },
   black: {
